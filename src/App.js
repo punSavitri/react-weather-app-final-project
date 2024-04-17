11f6d7a56bf3310f7548542e3c683154a22b2005
punSavitri <punshakshi@gmail.com>
2024-04-18 00:01:58 +0100

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React Weather App</h1>
+      <h1 className='text-warning' >Hello React Weather App</h1>
       <footer>
         <p>This project was coded by <a href="https://github.com/punSavitri" title="GitHub Repo link"> Savitri Pun </a> and <a href="https://github.com/punSavitri/react-weather-app-final-project.git" title="Link to GitHub project repo"> is a open-sourced </a> and <a href="https://react-forecast-weather-app2024.netlify.app/" title="Netlify link to the project"> hosted on Netlify</a> </p>
       </footer>
