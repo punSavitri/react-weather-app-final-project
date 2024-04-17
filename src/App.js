f6e55109e6585d255a1846f0844f32fec93bfe49
punSavitri <punshakshi@gmail.com>
2024-04-17 23:45:36 +0100

    added Netlify link to the project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello React Weather App</h1>
       <footer>
-        <p>This project was coded by <a href="https://github.com/punSavitri" title="GitHub Repo link">Savitri Pun </a> and <a href="https://github.com/punSavitri/react-weather-app-final-project.git" title="Link to GitHub project repo">is a open-sourced </a> </p>
+        <p>This project was coded by <a href="https://github.com/punSavitri" title="GitHub Repo link"> Savitri Pun </a> and <a href="https://github.com/punSavitri/react-weather-app-final-project.git" title="Link to GitHub project repo"> is a open-sourced </a> and <a href="https://react-forecast-weather-app2024.netlify.app/" title="Netlify link to the project"> hosted on Netlify</a> </p>
       </footer>
     </div>
   );
