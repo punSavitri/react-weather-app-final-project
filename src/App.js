799dd4b1d4753fa78084b0b6acabf5c13a143c9f
punSavitri <punshakshi@gmail.com>
2024-04-17 23:21:11 +0100

    added link to Github repo in the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React Weather App</h1>
+      <footer>
+        <p>This project was coded by <a href='https://github.com/punSavitri/' title='GitHub Repo link'>Savitri Pun </a> and <a href='https://github.com/punSavitri/react-weather-app-final-project.git' title='Link to GitHub project repo'>is a open-sourced </a> and <a href='#'>hosted on Netlify</a></p>
+      </footer>
     </div>
   );
 }
