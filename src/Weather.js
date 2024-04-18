5c3121db2cc73bbe6a989746ccfd6083bd7e7547
punSavitri <punshakshi@gmail.com>
2024-04-19 00:58:43 +0100

    added react animated weather custom  icon

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000)
     });
   }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,7 +14,8 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex">
             <div>
-              <img src={props.data.iconUrl} alt={props.data.description} />
+              <WeatherIcon code={props.data.icon} />
+
             </div>
             <div>
               <span className="temperature">{props.data.temperature}</span>
