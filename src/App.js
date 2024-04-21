a37e007b551b543e8188f19b8818de1a853f6b68
punSavitri <punshakshi@gmail.com>
2024-04-21 18:27:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Weather defaultCity="Tokyo" />
       <footer>
-        <p>This project was coded by <a href="https://github.com/punSavitri" title="GitHub Repo link" rel="noopener noreferrer"> Savitri Pun </a> and <a href="https://github.com/punSavitri/react-weather-app-final-project.git" title="Link to GitHub project repo" rel="noopener noreferrer"> is a open-sourced on GitHub </a> and <a href="https://react-forecast-weather-app2024.netlify.app/" title="Netlify link to the project" rel="noopener noreferrer"> hosted on Netlify</a>
+        <p>This project was coded by <a href="https://github.com/punSavitri" title="GitHub Repo link" > Savitri Pun </a> and <a href="https://github.com/punSavitri/react-weather-app-final-project.git" title="Link to GitHub project repo" > is a open-sourced on GitHub </a> and <a href="https://react-forecast-weather-app2024.netlify.app/" title="Netlify link to the project" > hosted on Netlify</a>
         </p>
       </footer>
     </div>
